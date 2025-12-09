3a7272c06810d4f95d11fc6dc22a8864e10675b0
AyushVerma2004 <vermaayush20180122139@gmail.com>
2025-12-09 09:39:29 +0530

    js file edited

--- Project/Frontend/Backend/Firebase.js
+++ Project/Frontend/Backend/Firebase.js
@@ -83,7 +83,7 @@ window.login = async function () {
   try {
     await signInWithEmailAndPassword(auth, email, password);
     alert("Login Successful!");
-    window.location.href = "Afterlogin.html";
+    window.location.href = "homepage.html";
   } catch (error) {
     alert("Login Failed: " + error.message);
   }
@@ -110,7 +110,7 @@ window.googleLogin = async function () {
     );
 
     alert("Welcome " + user.displayName + "!");
-    window.location.href = "Afterlogin.html";
+    window.location.href = "homepage.html";
 
   } catch (error) {
     alert("Google Sign-in Failed: " + error.message);
